1af0903631dcbce9b8c3712e010591dd49171064
Gönczi-Kovács Attila <rogkacs@gmail.com>
2021-10-23 21:26:24 +0200

    Készíts egy day01.Human nevű osztályt. Attribútumai legyenek egy név és egy életkor. Legyen egy konstruktor ami beállítja ezeket az értékeket. Készíts getter metódusokat az attribútumokhoz. A megoldásod a java-sv2-daily-labs02 töltsd fel, a projekt neve legyen week02-lab-tasks.

---
+++ day01/HumanMain.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class HumanMain {
+    public static void main(String[] args) {
+        Human human = new Human( "Sándor",42);
+        System.out.println(human.getName());
+        System.out.println(human.getAge());
+    }
+}
